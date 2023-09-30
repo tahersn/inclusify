05fcd61a0d1b73ee79c787dba76d1b4fc9a80393
Rayen Bourguiba <rayen.bourguiba@esprit.tn>
2023-09-30 14:14:29 +0100

    Categories list init

--- marketplace-service/src/main/java/tn/esprit/marketplaceservice/MarketplaceServiceApplication.java
+++ marketplace-service/src/main/java/tn/esprit/marketplaceservice/MarketplaceServiceApplication.java
@@ -22,9 +22,11 @@ public class MarketplaceServiceApplication {
 	@Bean
 	ApplicationRunner init(){
 		return (args) -> {
-			categoryRepository.save(new Category("Educational","This is an educational product"));
-			categoryRepository.save(new Category("Entertainment","This is an entertainment product"));
+			categoryRepository.save(new Category("Electronics","Explore cutting-edge gadgets and devices, from smartphones to smart home appliances, designed to simplify your life and keep you connected."));
+			categoryRepository.save(new Category("Fashion & Apparel","Stay stylish with our latest fashion collections. Discover trendy clothing, accessories, and footwear, curated to enhance your wardrobe and express your unique style."));
+			categoryRepository.save(new Category("Home & Living","Transform your living space into a haven of comfort and aesthetics. Find a wide range of furniture, decor, kitchen essentials, and home accessories that blend functionality with elegance."));
+			categoryRepository.save(new Category("Health & Wellness","Prioritize your well-being with our health and wellness products. From vitamins and supplements to fitness equipment and self-care essentials, we offer everything you need to lead a healthy lifestyle."));
+			categoryRepository.save(new Category("Books & Stationery","Immerse yourself in the world of literature and creativity. Explore an extensive collection of books, stationery items, and art supplies that inspire learning, creativity, and productivity."));
 		};
 	}
-
 }
