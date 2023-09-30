a7294ccd33e1d82f2ba743b2a28ea4534d668b96
Rayen Bourguiba <rayen.bourguiba@esprit.tn>
2023-09-30 13:06:22 +0100

    Repositories

---
+++ marketplace-service/src/main/java/tn/esprit/marketplaceservice/Product/ProductRepository.java
@@ -0,0 +1,6 @@
+package tn.esprit.marketplaceservice.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
